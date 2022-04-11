d180c4d99454742a8ef800c44f2fcaad2461816b
SH <673545134@qq.com>
2022-04-11 18:48:32 +0800

    feat(main): 基本架子

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,9 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    name: "skye"
+  },
   getters: {},
   mutations: {},
   actions: {},
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorldVue />
   </div>
 </template>
 <script lang="ts">
+import HelloWorldVue from "@/components/HelloWorld.vue";
 export default {
   name: "homeViews",
-  faga1: "2"
+  faga1: "2",
+  components: {
+    HelloWorldVue
+  }
 };
 </script>
