fbb20e056f57c0315ebb43ecf5357b580b246acb
SH <673545134@qq.com>
2022-04-12 16:13:47 +0800

    style: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,14 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto"
+      }
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "linebreak-style": [0, "error", "windows"]
   }
 }
